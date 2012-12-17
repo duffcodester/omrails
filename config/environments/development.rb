3f3fe57b1cb696f10d82996e478e73a387c4fbdd
Josh Duffy <jduffy@mines.edu>
2012-12-16 17:02:59 -0700

    changed host to heroku app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 Omrails::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.default_url_options = { :host => 'http://jduffy-omrails.herokuapp.com/' }
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
